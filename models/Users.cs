d76ba81e62b6a6154f9c727e4e4687158857bf21
leonTech254 <martinleontech23@gmail.com>
2023-12-17 08:42:51 +0300

    project initialization+[model]

---
+++ models/Users.cs
@@ -0,0 +1,13 @@
+namespace userModel
+	{
+	public class Users
+	{
+		public int Id { get; set; }
+		public String user_name { get; set; }
+		public string user_email { get; set; }
+		public string user_password { get; set;}
+		public string user_phone { get; set;}
+	}
+
+	}
+}
